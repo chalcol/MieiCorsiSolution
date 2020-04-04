a5cdfc72601e7167109db6bad53f428653b73144
chalcol <chalcol2001@live.com>
2020-04-04 11:01:07 +0200

    Aggiungere i file di progetto.

---
+++ MieiCorsi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MieiCorsi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
