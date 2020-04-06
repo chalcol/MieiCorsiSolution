811b392a3efbe58aa38597746da2e54447a264a0
chalcol <chalcol2001@live.com>
2020-04-06 15:13:54 +0200

    Creare controller e Aggiungere MiddleWare di routing

---
+++ MieiCorsi/Controllers/CorsiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MieiCorsi.Controllers
+{
+    public class CorsiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sono Index");
+        }
+
+        public IActionResult Details(String id)
+        {
+            return Content($"sono detail e ho ricevuto un {id}");
+        }
+    }
+}
--- MieiCorsi/Startup.cs
+++ MieiCorsi/Startup.cs
@@ -24,6 +24,8 @@ namespace MieiCorsi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,12 +48,20 @@ namespace MieiCorsi
 
             app.UseAuthorization();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
