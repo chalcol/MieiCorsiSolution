e22610145044bfe40ad2987852371e9d89a00d41
chalcol <chalcol2001@live.com>
2020-04-15 19:11:25 +0200

    Organizzare la logica all'interno del model

--- MieiCorsi/Controllers/CorsiController.cs
+++ MieiCorsi/Controllers/CorsiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MieiCorsi.Models.Services.Application;
+using MieiCorsi.Models.ViewModels;
 
 namespace MieiCorsi.Controllers
 {
@@ -10,7 +12,9 @@ namespace MieiCorsi.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var courseService = new CourseService();// Istanza della classe CourseService
+            List<CourseViewModel> courses = courseService.GetServices();//lista di corsi restituita dall'istanza della classe CourseService.
+            return View(courses);
         }
 
         public IActionResult Details(String id)
---
+++ MieiCorsi/Models/Services/Application/CourseService.cs
@@ -0,0 +1,16 @@
+﻿using MieiCorsi.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MieiCorsi.Models.Services.Application
+{
+    public class CourseService
+    {
+        public List<CourseViewModel> GetServices()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
